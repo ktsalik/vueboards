fa9d15f783e3fcba1f651bb5e655dfc4a3dd8674
Kostas Tsalikidis <tsalikidhs@gmail.com>
2023-04-15 13:25:11 +0300

    add Font Awesome package

--- src/main.js
+++ src/main.js
@@ -6,7 +6,15 @@ import router from './router'
 
 import './assets/main.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCircleNotch, faTimes } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faTimes)
+library.add(faCircleNotch)
+
 const app = createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
